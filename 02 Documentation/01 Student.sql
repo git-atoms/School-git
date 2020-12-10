56d37d5535b09ae9c80b3a939ef64f6655ecc841
git-atoms <atom.sliwinski@gmail.com>
2020-12-10 15:34:30 +0100

    Update 01 Student.sql
    Added description of my actions.

--- 02 Documentation/01 Student.sql
+++ 02 Documentation/01 Student.sql
@@ -1,11 +1,11 @@
 -- Create 'Student' table
-CREATE TABLE Student
+CREATE TABLE School_DB.dbo.Student 
 (
 id_studenta INT IDENTITY(1,1) NOT NULL,
 imie_studenta nvarchar(20) NOT NULL,
 nazwisko_studenta nvarchar(50) NOT NULL
 
-
+-- Setup of Primary Key (PK)
 CONSTRAINT PK_id_studenta
 PRIMARY KEY CLUSTERED
 )
