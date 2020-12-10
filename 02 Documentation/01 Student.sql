4768ce23584215e59980f87aec9da6f02f3670dd
git-atoms <atom.sliwinski@gmail.com>
2020-12-10 15:59:40 +0100

    Update 01 Student.sql
    I fixed logical mistake in syntax of table.

--- 02 Documentation/01 Student.sql
+++ 02 Documentation/01 Student.sql
@@ -1,11 +1,12 @@
 -- Create 'Student' table
 CREATE TABLE School_DB.dbo.Student 
 (
-id_studenta INT IDENTITY(1,1) NOT NULL,
-imie_studenta nvarchar(20) NOT NULL,
-nazwisko_studenta nvarchar(50) NOT NULL
+id_studenta INT IDENTITY(1,1) NOT NULL
 
 -- Setup of Primary Key (PK)
 CONSTRAINT PK_id_studenta
-PRIMARY KEY CLUSTERED
+PRIMARY KEY CLUSTERED,
+
+imie_studenta nvarchar(20) NOT NULL,
+nazwisko_studenta nvarchar(50) NOT NULL
 )
