c4cf6c23a016c58e7c0d0cc806191e6ad54e0dfe
git-atoms <atom.sliwinski@gmail.com>
2020-12-10 15:31:53 +0100

    Update 01 Student.sql
    Create 'Student' table with correction of previous action.

--- 02 Documentation/01 Student.sql
+++ 02 Documentation/01 Student.sql
@@ -1,7 +1,11 @@
 -- Create 'Student' table
 CREATE TABLE Student
 (
-id_studenta INT IDENTITY(1,1) NOT NULL
+id_studenta INT IDENTITY(1,1) NOT NULL,
+imie_studenta nvarchar(20) NOT NULL,
+nazwisko_studenta nvarchar(50) NOT NULL
+
+
 CONSTRAINT PK_id_studenta
 PRIMARY KEY CLUSTERED
 )
