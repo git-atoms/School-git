6a193dfb35d6a7a095fb0c8913841e3f689ce3a8
git-atoms <atom.sliwinski@gmail.com>
2020-12-10 16:08:46 +0100

    Update 03 Nauczyciel.sql
    Created 'Nauczyciel' table with PK

--- 02 Documentation/03 Nauczyciel.sql
+++ 02 Documentation/03 Nauczyciel.sql
@@ -1,1 +1,12 @@
--- 
+-- Create 'Nauczyciel' table
+CREATE TABLE School_DB.dbo.Nauczyciel
+(
+id_nauczyciela INT IDENTITY(1,1) NOT NULL
+
+-- Setup Primary Key (PK)
+CONSTRAINT PK_id_nauczyciela
+PRIMARY KEY CLUSTERED,
+
+imie_nauczyciela nvarchar(20) NOT NULL,
+nazwisko_nauczyciela nvarchar(50) NOT NULL
+)
