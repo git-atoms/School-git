7a456447fc410ee355ceb5b891f9592ef4e8ce27
git-atoms <atom.sliwinski@gmail.com>
2020-12-11 08:49:14 +0100

    Update 04 Kierunek studiów.sql
    CHECK constraint for year (only digits, in this millenium and in this age).

--- 02 Documentation/04 Kierunek studiów.sql
+++ 02 Documentation/04 Kierunek studiów.sql
@@ -8,5 +8,5 @@ CONSTRAINT PK_id_studiow
 PRIMARY KEY CLUSTERED,
 
 nazwa_kierunku nvarchar(20) NOT NULL,
-rok_akademicki INT NOT NULL
+rok_akademicki char(4) CHECK (rok_akademicki like '[2][0][0-9][0-9]') NOT NULL
 )
