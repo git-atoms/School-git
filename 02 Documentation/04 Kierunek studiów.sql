654b89929601a1ff9553c1f8b6c31ce6a823a673
git-atoms <atom.sliwinski@gmail.com>
2020-12-11 08:54:02 +0100

    Update 04 Kierunek studiów.sql
    I added comment for this table.

--- 02 Documentation/04 Kierunek studiów.sql
+++ 02 Documentation/04 Kierunek studiów.sql
@@ -8,5 +8,13 @@ CONSTRAINT PK_id_studiow
 PRIMARY KEY CLUSTERED,
 
 nazwa_kierunku nvarchar(20) NOT NULL,
+
+/*
+CHECK here is for:
+1. Only four digits
+2. Start year 2000
+2. Only in this millenium (not e.g. year 3K)
+3. Only in this calendar age (max 99 years)
+*/
 rok_akademicki char(4) CHECK (rok_akademicki like '[2][0][0-9][0-9]') NOT NULL
 )
