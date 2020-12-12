8294964a9518dd39180bbd38876b554ac2e36187
git-atoms <atom.sliwinski@gmail.com>
2020-12-12 18:57:38 +0100

    Update 05 StudentPrzedmiot.sql
    Foreign Keys and their references

--- 02 Documentation/05 StudentPrzedmiot.sql
+++ 02 Documentation/05 StudentPrzedmiot.sql
@@ -1,5 +1,20 @@
 -- Create 'StudentPrzedmiot' bridge table
 
-/* I create this table to avoid many-to-many relationship */
-
 CREATE TABLE School_DB.dbo.StudentPrzedmiot
+
+/* I create this table to avoid many-to-many relationship.
+This table has no Primary Key but Foreign Keys only */
+
+
+(
+id_studenta INT NOT NULL,
+id_przemiotu INT NOT NULL,
+ocena INT CHECK (ocena LIKE '[2-5]')
+
+-- Ustalanie kluczy obcych i ich powiązań
+FOREIGN KEY (id_studenta)
+REFERENCES dbo.Student(id_studenta),
+
+FOREIGN KEY (id_przemiotu)
+REFERENCES dbo.Przedmiot(id_przemiotu)
+)
