aad4c12b94cbbeaaf7d731a8af4b6a4b0eba3515
git-atoms <atom.sliwinski@gmail.com>
2020-12-12 19:09:56 +0100

    Update 06 NauczycielPrzedmiot.sql
    Set up Foreign Keys and their references.

--- 02 Documentation/06 NauczycielPrzedmiot.sql
+++ 02 Documentation/06 NauczycielPrzedmiot.sql
@@ -1,2 +1,15 @@
 -- Create 'NauczycielPrzedmiot' bridge table
+CREATE TABLE School_DB.dbo.NauczycielPrzedmiot
 
+-- There are no Primary Key in this table
+(
+id_nauczyciela INT NOT NULL,
+id_przedmiotu INT NOT NULL
+
+-- I set up Foreing Keys and their references
+FOREIGN KEY (id_nauczyciela)
+REFERENCES dbo.Nauczyciel(id_nauczyciela),
+
+FOREIGN KEY (id_przedmiotu)
+REFERENCES dbo.Przedmiot(id_przedmiotu)
+)
