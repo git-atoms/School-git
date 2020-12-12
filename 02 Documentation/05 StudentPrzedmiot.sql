721d93d9596e2e298cee85bd3aef3ed0abdf5e15
git-atoms <atom.sliwinski@gmail.com>
2020-12-12 18:40:54 +0100

    Update 05 StudentPrzedmiot.sql

--- 02 Documentation/05 StudentPrzedmiot.sql
+++ 02 Documentation/05 StudentPrzedmiot.sql
@@ -1,2 +1,5 @@
 -- Create 'StudentPrzedmiot' bridge table
-CREATE TABLE SchoolDB.dbo.StudentPrzedmiot
+
+/* I create this table to avoid many-to-many relationship */
+
+CREATE TABLE School_DB.dbo.StudentPrzedmiot
