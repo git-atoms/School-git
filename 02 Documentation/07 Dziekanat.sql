bfc42a747332c5c37f0481ad9246b704bdb39b66
git-atoms <atom.sliwinski@gmail.com>
2020-12-13 14:38:25 +0100

    Update 07 Dziekanat.sql
    Cosmetic changes

--- 02 Documentation/07 Dziekanat.sql
+++ 02 Documentation/07 Dziekanat.sql
@@ -12,14 +12,14 @@ id_przedmiotu INT NOT NULL
 and their references */
 
 FOREIGN KEY (id_studiow)
-REFERENCES dbo.Kierunek_studiow(id_studiow),
+    REFERENCES dbo.Kierunek_studiow(id_studiow),
 
 FOREIGN KEY (id_studenta)
-REFERENCES dbo.Student(id_studenta),
+    REFERENCES dbo.Student(id_studenta),
 
 FOREIGN KEY (id_nauczyciela)
-REFERENCES dbo.Nauczyciel(id_nauczyciela),
+    REFERENCES dbo.Nauczyciel(id_nauczyciela),
 
 FOREIGN KEY (id_przedmiotu)
-REFERENCES dbo.Przedmiot(id_przedmiotu)
+    REFERENCES dbo.Przedmiot(id_przedmiotu)
 )
