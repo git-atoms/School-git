6bfc7a8957b478a21e76b481c56f94f7360953d5
git-atoms <atom.sliwinski@gmail.com>
2020-12-13 14:37:41 +0100

    Update 01 Student.sql
    Cosmetic changes

--- 02 Documentation/01 Student.sql
+++ 02 Documentation/01 Student.sql
@@ -8,6 +8,6 @@ id_studenta INT IDENTITY(1,1) NOT NULL
 CONSTRAINT PK_id_studenta
 PRIMARY KEY CLUSTERED,
 
-imie_studenta nvarchar(20) NOT NULL,
-nazwisko_studenta nvarchar(50) NOT NULL
+imie_studenta NVARCHAR(20) NOT NULL,
+nazwisko_studenta NVARCHAR(50) NOT NULL
 )
