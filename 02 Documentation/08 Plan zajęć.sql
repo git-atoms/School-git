adb9d3c289deb0e78472260befbafeb9637b7efd
git-atoms <atom.sliwinski@gmail.com>
2020-12-13 13:44:11 +0100

    Update 08 Plan zajęć.sql

--- 02 Documentation/08 Plan zajęć.sql
+++ 02 Documentation/08 Plan zajęć.sql
@@ -2,5 +2,22 @@
 CREATE TABLE School_DB.dbo.Plan_zajec
 (
 -- Create columns of this table
+id_przedmiotu INT NOT NULL,
+id_nauczyciela INT NOT NULL,
+id_studenta INT NOT NULL
+-- Create Foreign Keys for these columns
 
+FOREIGN KEY (id_przedmiotu)
+REFERENCES dbo.Przedmiot(id_przedmiotu),
+
+FOREIGN KEY (id_nauczyciela)
+REFERENCES dbo.Nauczyciel(id_nauczyciela),
+
+FOREIGN KEY (id_studenta)
+REFERENCES dbo.Student(id_studenta)
+
+-- Create rest of columns
+lekcja_time_start
+lekcja_time_end
+numer_sali INT NOT NULL
 )
