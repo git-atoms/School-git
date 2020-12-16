64d62fe581316ce2bd8c8e69ecceff69fb3c7ab8
git-atoms <atom.sliwinski@gmail.com>
2020-12-16 18:16:15 +0100

    #com Select Distinct Update
    Changed example on more simple to learn.

--- 02 Documentation/#com Select Distinct.sql
+++ 02 Documentation/#com Select Distinct.sql
@@ -1,17 +1,9 @@
-/****** Script for SelectTopNRows command from SSMS  ******/
---SELECT TOP (1000) [BusinessEntityID]
-SELECT DISTINCT
---      ,[PersonType]
---      ,[NameStyle]
---      ,[Title]
-	     [FirstName] as 'Imiê'
---      ,[MiddleName]
-		,[LastName] as 'Nazwisko'
---      ,[Suffix]
---      ,[EmailPromotion]
---      ,[AdditionalContactInfo]
---      ,[Demographics]
---      ,[rowguid]
---      ,[ModifiedDate]
-  FROM [AdventureWorks2014].[Person].[Person]
- ORDER BY Nazwisko
+select distinct
+	--FirstName as Imiê
+	LastName as Nazwisko
+
+from
+	Person.Person
+
+order by
+	Nazwisko asc
